38358cdd11447e23cbe8c4715c98122a81e185e7
novic30 <103877148+novic30@users.noreply.github.com>
2024-06-22 21:19:55 +0700

    Add files via upload

---
+++ avg.java
@@ -0,0 +1,12 @@
+
+public class avg {
+	public static void main(String[] args) {
+		float n1 = 5.55f;
+		float n2 = 2.00f;
+		float n3 = 10.32f;
+
+		float sum = n1 + n2 + n3;
+
+		System.out.println("Average of float numbers is: " + sum/3);
+	}
+}
